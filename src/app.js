5b6375f22985b4ac6c7b522360096931fac4f8d9
alinekachel <83847813+alinekachel@users.noreply.github.com>
2021-05-31 18:51:04 +0900

    Header JS added
    Implemented JavaScript to load current date and local data to the header

---
+++ src/app.js
@@ -0,0 +1,63 @@
+function convertMinutes(inputMinutes) {
+  if (inputMinutes < 10) {
+    inputMinutes = `0${inputMinutes}`;
+  }
+  return inputMinutes;
+}
+
+function getCurrentLocal() {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position);
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+
+    let degreeUnit = "metric";
+    let currentDegree = document.querySelector("#header-degree").innerHTML;
+    if (currentDegree === "°C") {
+      degreeUnit = "metric";
+    } else {
+      degreeUnit = "imperial";
+    }
+
+    let apiKey = "cb9c7365e24a5b0ca2daf7074587f771";
+
+    let latLonCall = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${degreeUnit}&appid=${apiKey}`;
+    console.log(latLonCall);
+
+    axios.get(latLonCall).then(function (response) {
+      console.log(response);
+      let displayedCity = document.querySelector("#header-location");
+      displayedCity.innerHTML = response.data.name;
+
+      let headerTemp = document.querySelector("#header-temp");
+      headerTemp.innerHTML = Math.round(response.data.main.temp);
+
+      let headerCond = document.querySelector("#header-condition");
+      headerCond.innerHTML = response.data.weather[0].main.toLowerCase();
+
+      let humidity = response.data.main.humidity;
+      let windSpeed = Math.round(response.data.wind.speed);
+
+      let extraInfo = document.querySelector("#header-extra-info");
+      extraInfo.innerHTML = `Humidity: ${humidity}%, Wind: ${windSpeed} km/h`;
+    });
+  });
+}
+
+getCurrentLocal();
+
+let week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let today = new Date();
+let hourSite = document.querySelector("#current-day-hour");
+hourSite.innerHTML = `${today.getHours()}:${convertMinutes(
+  today.getMinutes()
+)} ${week[today.getDay()]}`;
