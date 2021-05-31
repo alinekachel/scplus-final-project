a99af200a0bffc6794febd9e13b22fd211ac73a9
alinekachel <83847813+alinekachel@users.noreply.github.com>
2021-05-31 19:14:48 +0900

    Search engine implemented

--- src/app.js
+++ src/app.js
@@ -44,6 +44,42 @@ function getCurrentLocal() {
   });
 }
 
+function displayCity(event) {
+  event.preventDefault();
+  let degreeUnit = "metric";
+
+  let currentDegree = document.querySelector("#header-degree").innerHTML;
+  if (currentDegree === "°C") {
+    degreeUnit = "metric";
+  } else {
+    degreeUnit = "imperial";
+  }
+
+  let apiKey = "cb9c7365e24a5b0ca2daf7074587f771";
+  let cityName = document.querySelector("#inserted-city").value;
+
+  let cityCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${degreeUnit}&appid=${apiKey}`;
+  console.log(cityCall);
+
+  axios.get(cityCall).then(function (response) {
+    console.log(response);
+    let displayedCity = document.querySelector("#header-location");
+    displayedCity.innerHTML = response.data.name;
+
+    let headerTemp = document.querySelector("#header-temp");
+    headerTemp.innerHTML = Math.round(response.data.main.temp);
+
+    let headerCond = document.querySelector("#header-condition");
+    headerCond.innerHTML = response.data.weather[0].main.toLowerCase();
+
+    let humidity = response.data.main.humidity;
+    let windSpeed = Math.round(response.data.wind.speed);
+
+    let extraInfo = document.querySelector("#header-extra-info");
+    extraInfo.innerHTML = `Humidity: ${humidity}%, Wind: ${windSpeed} km/h`;
+  });
+}
+
 getCurrentLocal();
 
 let week = [
@@ -61,3 +97,6 @@ let hourSite = document.querySelector("#current-day-hour");
 hourSite.innerHTML = `${today.getHours()}:${convertMinutes(
   today.getMinutes()
 )} ${week[today.getDay()]}`;
+
+let citySearch = document.querySelector("#search-city-form");
+citySearch.addEventListener("submit", displayCity);
